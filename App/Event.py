32b16eb2aad529d915ecff555b4349beff6a1ae3
エルフです <kimmyxyc@163.com>
2023-07-27 19:54:19 +0800

    Refine Code

--- App/Event.py
+++ App/Event.py
@@ -162,7 +162,7 @@ class JoinRequest:
 
         notice_message = await bot.send_message(
             self.chat_id,
-            f"{request.from_user.mention}(ID: {self.user_id}) is requesting to join this group."
+            f"{request.from_user.mention} (ID: {self.user_id}) is requesting to join this group."
             f"\n\ntg://user?id={self.user_id}",
             reply_markup=keyboard
         )
@@ -208,25 +208,25 @@ class JoinRequest:
             logger.info(f"{self.user_id}: No one voted in {self.chat_id}")
             result_message = await notice_message.reply("No one voted.")
             approve_user = False
-            edit_msg = f"{request.from_user.mention}(ID: {self.user_id}): No one voted."
+            edit_msg = f"{request.from_user.mention} (ID: {self.user_id}): No one voted."
             user_reply_msg = "无人投票，请稍后尝试重新申请。\nNo one voted. Please request again later."
         elif allow_count > deny_count:
             logger.info(f"{self.user_id}: Approved {self.user_id} in {self.chat_id}")
             result_message = await notice_message.reply("Approved.")
             approve_user = True
-            edit_msg = f"{request.from_user.mention}(ID: {self.user_id}): Approved."
+            edit_msg = f"{request.from_user.mention} (ID: {self.user_id}): Approved."
             user_reply_msg = "您已获批准加入\nYou have been approved."
         elif allow_count == deny_count:
             logger.info(f"{self.user_id}: Tie in {self.chat_id}")
             result_message = await notice_message.reply("Tie.")
             approve_user = False
-            edit_msg = f"{request.from_user.mention}(ID: {self.user_id}): Tie."
+            edit_msg = f"{request.from_user.mention} (ID: {self.user_id}): Tie."
             user_reply_msg = "平票，请稍后尝试重新申请。\nTie. Please request again later."
         else:
             logger.info(f"{self.user_id}: Denied {self.user_id} in {self.chat_id}")
             result_message = await notice_message.reply("Denied.")
             approve_user = False
-            edit_msg = f"{request.from_user.mention}(ID: {self.user_id}): Denied."
+            edit_msg = f"{request.from_user.mention} (ID: {self.user_id}): Denied."
             user_reply_msg = "您的申请已被拒绝。\nYou have been denied."
 
         try:
@@ -261,14 +261,14 @@ class JoinRequest:
         if action == "Approve":
             await self.request.approve()
             await bot.answer_callback_query(callback_query.id, "Approved.")
-            edit_msg = f"{self.request.from_user.mention}(ID: {self.user_id}): " \
+            edit_msg = f"{self.request.from_user.mention} (ID: {self.user_id}): " \
                        f"Approved by {callback_query.from_user.mention}"
             reply_msg = "您已获批准加入\nYou have been approved."
             self.finished = True
         elif action == "Reject":
             await self.request.decline()
             await bot.answer_callback_query(callback_query.id, "Denied.")
-            edit_msg = f"{self.request.from_user.mention}(ID: {self.user_id}): " \
+            edit_msg = f"{self.request.from_user.mention} (ID: {self.user_id}): " \
                        f"Denied by {callback_query.from_user.mention}"
             reply_msg = "您的申请已被拒绝。\nYou have been denied."
             self.finished = True
@@ -278,14 +278,14 @@ class JoinRequest:
                     await self.request.decline()
                     await bot.kick_chat_member(self.chat_id, self.user_id)
                     await bot.answer_callback_query(callback_query.id, "Banned.")
-                    edit_msg = f"{self.request.from_user.mention}(ID: {self.user_id}): " \
+                    edit_msg = f"{self.request.from_user.mention} (ID: {self.user_id}): " \
                                f"Banned by {callback_query.from_user.mention}"
                     reply_msg = "您已被封禁。\nYou have been banned."
                     self.finished = True
                 else:
                     await self.request.decline()
                     await bot.answer_callback_query(callback_query.id, "Bot has no permission to ban.")
-                    edit_msg = f"{self.request.from_user.mention}(ID: {self.user_id}): " \
+                    edit_msg = f"{self.request.from_user.mention} (ID: {self.user_id}): " \
                                f"Denied by {callback_query.from_user.mention}"
                     reply_msg = "您的申请已被拒绝。\nYou have been denied."
                     self.finished = True
@@ -314,15 +314,20 @@ class Ostracism:
         self.user_id = user_id
         self.bot_id = bot_id
         self.target_id = target_id
+
         self.admin_status = False
-        self.start_msg = None
-        self.ostracism_id = None
-        self.target_member = None
-        self.polling = None
-        self.bot_member = None
         self.cancelled = False
 
+        self.ostracism_id = None
+        self.start_msg = None
+        self.polling = None
+        self.cmd_msg = None
+
+        self.target_member = None
+        self.bot_member = None
+
     async def start_ostracism(self, bot, message):
+        self.cmd_msg = message
         self.bot_member = await bot.get_chat_member(self.chat_id, self.bot_id)
         try:
             self.target_member = await bot.get_chat_member(self.chat_id, self.target_id)
@@ -336,6 +341,7 @@ class Ostracism:
         elif self.target_member.status == 'none':
             await message.reply("Cannot find the target user.")
             return
+
         self.ostracism_id = calculate_md5(f"{self.chat_id}@{self.user_id}@{int(self.target_id)}@Ostracism")
         keyboard = types.InlineKeyboardMarkup(row_width=2)
         approve_button = types.InlineKeyboardButton(
@@ -347,11 +353,13 @@ class Ostracism:
             callback_data=f"Ostracism/Cancel/{self.ostracism_id}"
         )
         keyboard.add(approve_button, cancel_button)
+
         self.start_msg = await message.reply(
             f"{message.from_user.mention} want to start Ostracism voting to user {self.target_member.user.mention}."
             f"\n\nTarget User: tg://user?id={self.target_id}",
             reply_markup=keyboard
         )
+
         await asyncio.sleep(300)
         if not self.admin_status:
             await self.start_msg.edit_text("No one approve this Ostracism.")
@@ -372,32 +380,33 @@ class Ostracism:
             self.admin_status = True
         elif action == "Cancel":
             self.admin_status = True
-            await bot.answer_callback_query(callback_query.id, "Canceled.")
-            await self.start_msg.edit_text(f"Ostracism was canceled by {callback_query.from_user.mention}.")
-            return
-        elif action == "CancelKill":
             self.cancelled = True
             await bot.answer_callback_query(callback_query.id, "Canceled.")
             await self.start_msg.edit_text(
-                f"Ostracism voting to user {self.target_member.user.mention} was canceled"
-                f" by {callback_query.from_user.mention}."
+                f"Ostracism voting to user {self.target_member.user.mention} was canceled "
+                f"by {callback_query.from_user.mention}."
             )
-            await bot.delete_message(chat_id=self.chat_id, message_id=self.polling.message_id)
+            if self.polling:
+                await bot.delete_message(chat_id=self.chat_id, message_id=self.polling.message_id)
             return
         else:
             await bot.answer_callback_query(callback_query.id, "Unknown action.")
             logger.error(f"Unknown action: {action}")
             return
+
         keyboard = types.InlineKeyboardMarkup(row_width=1)
         cancel_button = types.InlineKeyboardButton(
             text="Cancel",
-            callback_data=f"Ostracism/CancelKill/{self.ostracism_id}"
+            callback_data=f"Ostracism/Cancel/{self.ostracism_id}"
         )
         keyboard.add(cancel_button)
         await self.start_msg.edit_text(
-            f"Start Ostracism voting to user {self.target_member.user.mention}.",
+            f"Start Ostracism voting to user {self.target_member.user.mention}."
+            f"\nInitiator: {self.cmd_msg.from_user.mention} Approver: {callback_query.from_user.mention}."
+            f"\n\nTarget User: tg://user?id={self.target_id}",
             reply_markup=keyboard
         )
+
         vote_question = "Kick out this user?"
         vote_options = ["Yes", "No"]
         self.polling = await bot.send_poll(
@@ -428,6 +437,7 @@ class Ostracism:
                 chat_id=self.chat_id,
                 message_id=self.polling.message_id,
             )
+
         vote_message = await bot.stop_poll(self.chat_id, self.polling.message_id)
         allow_count = vote_message.options[0].voter_count
         deny_count = vote_message.options[1].voter_count
@@ -436,23 +446,25 @@ class Ostracism:
             logger.info(f"Ostracism {self.target_id}: No one voted in {self.chat_id}")
             result_message = await self.start_msg.reply("No one voted.")
             kick_user = False
-            edit_msg = f"Ostracism {self.target_member.user.mention}(ID: {self.target_id}): No one voted."
+            edit_msg = f"Ostracism {self.target_member.user.mention} (ID: {self.target_id}): No one voted."
         elif allow_count > deny_count:
             logger.info(f"Ostracism {self.target_id}: Kick {self.user_id} in {self.chat_id}")
             result_message = await self.start_msg.reply("Kick out.")
             kick_user = True
-            edit_msg = f"Ostracism {self.target_member.user.mention}(ID: {self.target_id}): Kick out."
+            edit_msg = f"Ostracism {self.target_member.user.mention} (ID: {self.target_id}): Kick out."
         elif allow_count == deny_count:
             logger.info(f"Ostracism {self.target_id}: Tie in {self.chat_id}")
             result_message = await self.start_msg.reply("Tie.")
             kick_user = False
-            edit_msg = f"Ostracism {self.target_member.user.mention}(ID: {self.target_id}): Tie."
+            edit_msg = f"Ostracism {self.target_member.user.mention} (ID: {self.target_id}): Tie."
         else:
             logger.info(f"Ostracism {self.target_id}: Don't kick out {self.user_id} in {self.chat_id}")
             result_message = await self.start_msg.reply("Don't kick out.")
             kick_user = False
-            edit_msg = f"Ostracism {self.target_member.user.mention}(ID: {self.target_id}): Don't kick out."
+            edit_msg = f"Ostracism {self.target_member.user.mention} (ID: {self.target_id}): Don't kick out."
+
         await self.start_msg.edit_text(edit_msg)
+
         if kick_user:
             await bot.kick_chat_member(self.chat_id, self.target_id)
             permissions = types.ChatPermissions(
