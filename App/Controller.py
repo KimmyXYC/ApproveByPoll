045cdc978c3379db92e8c6d079c2f443b9912fa0
KimmyXYC <kimmyxyc@gmail.com>
2024-02-05 21:10:06 +0800

    fix: Remove Ostracism support

--- App/Controller.py
+++ App/Controller.py
@@ -8,7 +8,6 @@ from loguru import logger
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from App import Event
-from App.Ostracism import Ostracism
 from App.JoinRequest import JoinRequest
 from utils.Tool import cal_md5
 
@@ -18,10 +17,9 @@ class BotRunner(object):
         self.bot = config.bot
         self.proxy = config.proxy
         self.config = config
-        self.db = db
-        self.request_tasks = {}
-        self.ostracism_tasks = {}
         self.bot_id = self.bot.botToken.split(":")[0]
+        self.db = db
+        self.join_tasks = {}  # Dict used to store join requests
 
     def run(self):
         logger.success("Bot Start")
@@ -45,13 +43,6 @@ class BotRunner(object):
             else:
                 await message.reply("Please use this command in the group.")
 
-        @dp.message_handler(commands=["clean_pin_service_msg"])
-        async def handle_command_clean_pin_msg(message: types.Message):
-            if message.chat.type in ["group", "supergroup"]:
-                await Event.set_clean_pin_service_msg(bot, message, self.db, self.bot_id)
-            else:
-                await message.reply("Please use this command in the group.")
-
         @dp.message_handler(commands=["set_vote_time"])
         async def handle_command_set_vote_time(message: types.Message):
             if message.chat.type in ["group", "supergroup"]:
@@ -59,48 +50,10 @@ class BotRunner(object):
             else:
                 await message.reply("Please use this command in the group.")
 
-        @dp.message_handler(commands=["set_ostracism"])
-        async def handle_command_set_ostracism(message: types.Message):
+        @dp.message_handler(commands=["clean_pin_service_msg"])
+        async def handle_command_clean_pin_msg(message: types.Message):
             if message.chat.type in ["group", "supergroup"]:
-                await Event.set_ostracism(bot, message, self.db, self.bot_id)
-            else:
-                await message.reply("Please use this command in the group.")
-
-        @dp.message_handler(commands=["ostracism"])
-        async def handle_command_ostracism(message: types.Message):
-            if message.chat.type in ["group", "supergroup"]:
-                status = self.db.get(str(message.chat.id))
-                if status:
-                    if not status.get("ostracism", False):
-                        await message.reply("The ostracism function is not enabled")
-                        return
-                else:
-                    await message.reply("The ostracism function is not enabled")
-                    return
-                command_args = message.text.split()
-                if len(command_args) == 1:
-                    if message.reply_to_message is not None:
-                        ostracism_id = cal_md5(
-                            f"{message.chat.id}@{message.from_user.id}"
-                            f"@{message.reply_to_message.from_user.id}@Ostracism"
-                        )
-                        ostracism_task = Ostracism(message.chat.id, message.from_user.id,
-                                                   message.reply_to_message.from_user.id, self.bot_id)
-                        self.ostracism_tasks[ostracism_id] = ostracism_task
-                        await ostracism_task.start_ostracism(bot, message)
-                    else:
-                        await message.reply("Malformed, please reply to the message sent by the user you want to "
-                                            "ostracize or use the /ostracism command followed by the user's ID.")
-                elif len(command_args) == 2:
-                    ostracism_id = cal_md5(
-                        f"{message.chat.id}@{message.from_user.id}@{int(command_args[1])}@Ostracism")
-                    ostracism_task = Ostracism(message.chat.id, message.from_user.id,
-                                               command_args[1], self.bot_id)
-                    self.ostracism_tasks[ostracism_id] = ostracism_task
-                    await ostracism_task.start_ostracism(bot, message)
-                else:
-                    await message.reply("Malformed, please reply to the message sent by the user you want to "
-                                        "ostracize or use the /ostracism command followed by the user's ID.")
+                await Event.set_clean_pin_service_msg(bot, message, self.db, self.bot_id)
             else:
                 await message.reply("Please use this command in the group.")
 
@@ -117,23 +70,16 @@ class BotRunner(object):
 
         @dp.chat_join_request_handler()
         async def handle_new_chat_members(request: types.ChatJoinRequest):
-            join_request_id = cal_md5(f"{request.chat.id}@{request.from_user.id}@Join")
-            request_task = JoinRequest(request.chat.id, request.from_user.id, self.bot_id, self.config)
-            self.request_tasks[join_request_id] = request_task
-            await request_task.handle_join_request(bot, request, self.db)
+            join_request_id = cal_md5(f"{request.chat.id}@{request.from_user.id}")
+            join_task = JoinRequest(request.chat.id, request.from_user.id, self.bot_id, self.config)
+            self.join_tasks[join_request_id] = join_task
+            await join_task.handle_join_request(bot, request, self.db)
 
         @dp.callback_query_handler(lambda c: True)
         async def handle_callback_query(callback_query: types.CallbackQuery):
-            requests_type = callback_query.data.split("/")[0]
-            if requests_type == "Join":
-                action = callback_query.data.split("/")[1]
-                join_request_id = callback_query.data.split("/")[2]
-                request_task = self.request_tasks.get(join_request_id)
-                await request_task.handle_button(bot, callback_query, action)
-            elif requests_type == "Ostracism":
-                action = callback_query.data.split("/")[1]
-                ostracism_id = callback_query.data.split("/")[2]
-                ostracism_task = self.ostracism_tasks.get(ostracism_id)
-                await ostracism_task.handle_button(bot, callback_query, action, self.db)
+            action = callback_query.data.split()[0]
+            join_request_id = callback_query.data.split()[1]
+            join_tasks = self.join_tasks.get(join_request_id)
+            await join_tasks.handle_button(bot, callback_query, action)
 
         asyncio.run(dp.start_polling())
--- App/JoinRequest.py
+++ App/JoinRequest.py
@@ -27,31 +27,35 @@ class JoinRequest:
 
     async def handle_join_request(self, bot, request: types.ChatJoinRequest, db):
         self.request = request
-
         self.bot_member = await bot.get_chat_member(self.chat_id, self.bot_id)
-        logger.info(f"New join request from {request.from_user.mention}(ID: {self.user_id}) in {self.chat_id}")
 
+        # Log
+        logger.info(f"New join request from {request.from_user.mention}(ID: {self.user_id}) in {self.chat_id}")
         await log_c(bot, request, "JoinRequest", self.config.log)
 
         chat_dict = db.get(str(self.chat_id))
         status_pin_msg = chat_dict.get("pin_msg", False)
         vote_time = chat_dict.get("vote_time", 600)
 
+        # Time format
         minutes = vote_time // 60
         seconds = vote_time % 60
         if minutes == 0:
             _cn_time = f"{seconds}秒"
             _en_time = f"{seconds} seconds"
+        elif seconds == 0:
+            _cn_time = f"{minutes}分钟"
+            _en_time = f"{minutes} minutes"
         else:
             _cn_time = f"{minutes}分钟{seconds}秒"
             _en_time = f"{minutes} minutes and {seconds} seconds"
 
+        # Send message to user
         _zh_info = f"您正在申请加入「{request.chat.title}」，结果将于 {_cn_time} 后告知您。"
         _en_info = f"You are applying to join 「{request.chat.title}」. " \
                    f"The result will be communicated to you in {_en_time}."
         _info = f"请激活机器人以便告知您申请结果。\n" \
                 f"Please activate the robot so that it can inform you of the application result."
-
         try:
             self.user_message = await bot.send_message(
                 self.user_id,
@@ -60,11 +64,12 @@ class JoinRequest:
         except Exception as e:
             logger.error(f"Cannot send message to {self.user_id}: {e}")
 
-        join_request_id = cal_md5(f"{self.chat_id}@{self.user_id}@Join")
+        # Buttons
+        join_request_id = cal_md5(f"{self.chat_id}@{self.user_id}")
         keyboard = types.InlineKeyboardMarkup(row_width=3)
-        approve_button = types.InlineKeyboardButton(text="Approve", callback_data=f"Join/Approve/{join_request_id}")
-        reject_button = types.InlineKeyboardButton(text="Reject", callback_data=f"Join/Reject/{join_request_id}")
-        ban_button = types.InlineKeyboardButton(text="Ban", callback_data=f"Join/Ban/{join_request_id}")
+        approve_button = types.InlineKeyboardButton(text="Approve", callback_data=f"Approve {join_request_id}")
+        reject_button = types.InlineKeyboardButton(text="Reject", callback_data=f"Reject {join_request_id}")
+        ban_button = types.InlineKeyboardButton(text="Ban", callback_data=f"Ban {join_request_id}")
         keyboard.add(approve_button, reject_button, ban_button)
 
         notice_message = await bot.send_message(
@@ -75,6 +80,7 @@ class JoinRequest:
         )
         self.notice_message = notice_message
 
+        # Polling
         vote_question = "Approve this user?"
         vote_options = ["Yes", "No"]
         self.polling = await bot.send_poll(
@@ -95,19 +101,22 @@ class JoinRequest:
 
         await asyncio.sleep(vote_time)
 
+        # Check if the request has been processed
         if self.finished:
             return
+
         if status_pin_msg and self.bot_member.status == 'administrator' and self.bot_member.can_pin_messages:
             await bot.unpin_chat_message(
                 chat_id=self.chat_id,
                 message_id=self.polling.message_id,
             )
 
+        # Get vote result
         vote_message = await bot.stop_poll(request.chat.id, self.polling.message_id)
-
         allow_count = vote_message.options[0].voter_count
         deny_count = vote_message.options[1].voter_count
 
+        # Process the vote result
         if vote_message.total_voter_count == 0:
             logger.info(f"{self.user_id}: No one voted in {self.chat_id}")
             result_message = await notice_message.reply("No one voted.")
@@ -133,6 +142,7 @@ class JoinRequest:
             edit_msg = f"{request.from_user.mention} (ID: {self.user_id}): Denied."
             user_reply_msg = "您的申请已被拒绝。\nYou have been denied."
 
+        # Process the request
         try:
             if approve_user:
                 await log_c(bot, request, "Approve_JoinRequest", self.config.log)
@@ -142,10 +152,7 @@ class JoinRequest:
                 await request.decline()
         except Exception as e:
             logger.error(f"User_id:{self.user_id} in Chat_id:{self.chat_id}: {e}")
-        try:
-            await notice_message.edit_text(edit_msg)
-        except Exception as e:
-            logger.error(f"User_id:{self.user_id} in Chat_id:{self.chat_id}: {e}")
+        await notice_message.edit_text(edit_msg)
         try:
             await self.user_message.reply(user_reply_msg)
         except Exception as e:
@@ -153,20 +160,20 @@ class JoinRequest:
 
         await asyncio.sleep(60)
 
-        try:
-            await bot.delete_message(chat_id=request.chat.id, message_id=self.polling.message_id)
-            await bot.delete_message(chat_id=request.chat.id, message_id=result_message.message_id)
-        except Exception as e:
-            logger.error(f"User_id:{self.user_id} in Chat_id:{self.chat_id}: {e}")
+        # Clean up
+        await bot.delete_message(chat_id=request.chat.id, message_id=self.polling.message_id)
+        await bot.delete_message(chat_id=request.chat.id, message_id=result_message.message_id)
 
     async def handle_button(self, bot, callback_query: types.CallbackQuery, action):
         chat_member = await bot.get_chat_member(self.chat_id, callback_query.from_user.id)
+
+        # Check permission
         if not ((chat_member.status == 'administrator' and chat_member.can_invite_users) or
                 chat_member.status == 'creator'):
             await bot.answer_callback_query(callback_query.id, "You have no permission to do this.")
             return
-        reply_msg = None
-        edit_msg = None
+
+        # Process the request
         if self.finished:
             await bot.answer_callback_query(callback_query.id, "This request has been processed")
             return
@@ -207,18 +214,18 @@ class JoinRequest:
                     reply_msg = "您的申请已被拒绝。\nYou have been denied."
             else:
                 await bot.answer_callback_query(callback_query.id, "You have no permission to ban.")
+                return
         else:
             await bot.answer_callback_query(callback_query.id, "Unknown action.")
             logger.error(f"Unknown action: {action}")
-
-        if not self.finished:
             return
-        if edit_msg:
-            await self.notice_message.edit_text(edit_msg)
+
+        await self.notice_message.edit_text(edit_msg)
         try:
-            if reply_msg:
-                await self.user_message.reply(reply_msg)
+            await self.user_message.reply(reply_msg)
         except Exception as e:
             logger.error(f"User_id:{self.user_id} in Chat_id:{self.chat_id}: {e}")
+
+        # Clean up
         await bot.stop_poll(self.chat_id, self.polling.message_id)
         await bot.delete_message(chat_id=self.chat_id, message_id=self.polling.message_id)
